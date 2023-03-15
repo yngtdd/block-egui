763e18d6104db20204a2b41fae6aaf7d6cfd9519
yngtdd <yngtdd@gmail.com>
2023-03-14 19:10:35 -0700

    feat: Add test plot at topbar

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,33 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        catppuccin_egui::set_theme(&ctx, catppuccin_egui::MACCHIATO);
+
+        egui::TopBottomPanel::top("vis panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                example_plot(ui);
+            });
+
         egui::CentralPanel::default().show(ctx, |ui| {
         });
     }
 }
 
+fn example_plot(ui: &mut egui::Ui) -> egui::Response {
+    use egui::plot::{Line, PlotPoints};
+    let n = 128;
+    let line_points: PlotPoints = (0..=n)
+        .map(|i| {
+            use std::f64::consts::TAU;
+            let x = egui::remap(i as f64, 0.0..=n as f64, -TAU..=TAU);
+            [x, x.sin()]
+        })
+        .collect();
+    let line = Line::new(line_points);
+    egui::plot::Plot::new("example_plot")
+        .height(300.0)
+        .data_aspect(1.0)
+        .show(ui, |plot_ui| plot_ui.line(line))
+        .response
+}
