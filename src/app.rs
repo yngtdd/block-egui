e8fedf5d9781c8bffb75b80d53d340b4c0dadb6b
yngtdd <yngtdd@gmail.com>
2023-03-14 22:09:36 -0700

    feat: Add the node data and parameters

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,49 @@
-use eframe::{App, egui};
+use eframe::{egui, App};
+use egui_node_graph::*;
+use serde::{Deserialize, Serialize};
+
+/// Data stored in each of the nodes
+///
+/// Useful to store additional data that does not live in parameters
+#[cfg_attr(feature = "persistence", derive(Deserialize, Serialize))]
+pub struct NodeData {
+    weibull_cdf: Vec<f64>,
+}
+
+/// Node input parameters
+///
+/// Used to create Weibull distribution for the node
+///
+/// # Note:
+/// There is a correspondence between the `scale` and
+/// the number of `time_steps`. Reliability engineers 
+/// using this tool will intuitivedly consider how the
+/// scale of the Weibull distribution stretches out its
+/// CDF over time. This time must then be reflected in 
+/// the number of time steps. Think of `time_steps` as 
+/// unitless measure of time that relates to the Weibull's scale
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "persistence", derive(Deserialize, Serialize))]
+pub struct NodeParameters {
+    /// Weibull shape
+    shape: f64,
+    /// Weibull scale
+    scale: f64,
+    /// Number of time steps to draw the CDF over
+    time_steps: u32,
+}
+
+impl Default for NodeParameters {
+    fn default() -> Self {
+        Self {
+            shape: 0.5,
+            scale: 200.0,
+            time_steps: 730,
+        }
+    }
+}
+
+
 
 pub struct MyApp {}
 
@@ -24,6 +69,7 @@ impl App for MyApp {
     }
 }
 
+// Todo(Todd): Replace this with plotting CDF functions
 fn example_plot(ui: &mut egui::Ui) -> egui::Response {
     use egui::plot::{Line, PlotPoints};
     let n = 128;
